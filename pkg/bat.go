8cef249c8d6648df15d79c5f2bdf6d85b22717f2
andrieee44 <andrieee44@gmail.com>
2024-07-03 00:35:18 +0800

    Add bat module

---
+++ pkg/bat.go
@@ -0,0 +1,66 @@
+package jstat
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+type Bat struct {
+	interval time.Duration
+	icons    []string
+}
+
+func (mod *Bat) Run() (json.RawMessage, error) {
+	type batInfo struct {
+		Name, Status, Icon string
+		Capacity           int
+	}
+
+	var (
+		batsPath []string
+		batsInfo []batInfo
+		path     string
+		status   []byte
+		capacity int
+		err      error
+	)
+
+	batsPath, err = filepath.Glob("/sys/class/power_supply/BAT*")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, path = range batsPath {
+		status, err = os.ReadFile(filepath.Join(path, "status"))
+		if err != nil {
+			return nil, err
+		}
+
+		capacity, err = fileAtoi(filepath.Join(path, "capacity"))
+		if err != nil {
+			return nil, err
+		}
+
+		batsInfo = append(batsInfo, batInfo{
+			Name:     filepath.Base(path),
+			Status:   string(status[:len(status)-1]),
+			Icon:     icon(mod.icons, 100, float64(capacity)),
+			Capacity: capacity,
+		})
+	}
+
+	return json.Marshal(batsInfo)
+}
+
+func (mod *Bat) Sleep() {
+	time.Sleep(mod.interval)
+}
+
+func NewBat(interval time.Duration, icons []string) *Bat {
+	return &Bat{
+		interval: interval,
+		icons:    icons,
+	}
+}
