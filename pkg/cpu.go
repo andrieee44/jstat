e901f4571cafc5714249c575cb4a19e2452db72e
andrieee44 <andrieee44@gmail.com>
2024-07-04 08:47:31 +0800

    cpu: Merge updateCores to Run method

--- pkg/cpu.go
+++ pkg/cpu.go
@@ -27,18 +27,83 @@ func (mod *Cpu) Init() error {
 
 func (mod *Cpu) Run() (json.RawMessage, error) {
 	var (
-		core     cpuCore
-		avgUsage float64
-		err      error
+		stat                                    *os.File
+		scanner                                 *bufio.Scanner
+		fields                                  []string
+		coreNStr, numStr                        string
+		idx, coreN, freq, num, idle, sum, delta int
+		avgUsage                                float64
+		ok                                      bool
+		err                                     error
 	)
 
-	err = mod.updateCores()
+	stat, err = os.Open("/proc/stat")
 	if err != nil {
 		return nil, err
 	}
 
-	for _, core = range mod.cores {
-		avgUsage += core.Usage
+	scanner = bufio.NewScanner(stat)
+
+	for scanner.Scan() {
+		fields = strings.Fields(scanner.Text())
+		if fields[0] == "cpu" {
+			continue
+		}
+
+		coreNStr, ok = strings.CutPrefix(fields[0], "cpu")
+		if !ok {
+			break
+		}
+
+		coreN, err = strconv.Atoi(coreNStr)
+		if err != nil {
+			return nil, err
+		}
+
+		if coreN == len(mod.cores) {
+			mod.cores = append(mod.cores, cpuCore{})
+		}
+
+		freq, err = fileAtoi(fmt.Sprintf("/sys/devices/system/cpu/cpu%d/cpufreq/scaling_cur_freq", coreN))
+		if err != nil {
+			return nil, err
+		}
+
+		idle = 0
+		sum = 0
+
+		for idx, numStr = range fields[1:] {
+			if idx == 7 {
+				break
+			}
+
+			num, err = strconv.Atoi(numStr)
+			if err != nil {
+				return nil, err
+			}
+
+			if idx == 3 || idx == 4 {
+				idle += num
+			}
+
+			sum += num
+		}
+
+		delta = sum - mod.cores[coreN].sum
+		mod.cores[coreN].Usage = float64(delta-(idle-mod.cores[coreN].idle)) / float64(delta) * 100
+		mod.cores[coreN].Freq = freq
+		mod.cores[coreN].idle = idle
+		mod.cores[coreN].sum = sum
+		avgUsage += mod.cores[coreN].Usage
+	}
+
+	if scanner.Err() != nil {
+		return nil, err
+	}
+
+	err = stat.Close()
+	if err != nil {
+		return nil, err
 	}
 
 	return json.Marshal(struct {
@@ -57,88 +122,6 @@ func (mod *Cpu) Sleep() error {
 }
 
 func (mod *Cpu) Cleanup() error {
-	return nil
-}
-
-func (mod *Cpu) updateCores() error {
-	var (
-		stat                                    *os.File
-		scanner                                 *bufio.Scanner
-		fields                                  []string
-		coreNStr, numStr                        string
-		idx, coreN, freq, num, idle, sum, delta int
-		ok                                      bool
-		err                                     error
-	)
-
-	stat, err = os.Open("/proc/stat")
-	if err != nil {
-		return err
-	}
-
-	scanner = bufio.NewScanner(stat)
-
-	for scanner.Scan() {
-		fields = strings.Fields(scanner.Text())
-		if fields[0] == "cpu" {
-			continue
-		}
-
-		coreNStr, ok = strings.CutPrefix(fields[0], "cpu")
-		if !ok {
-			break
-		}
-
-		coreN, err = strconv.Atoi(coreNStr)
-		if err != nil {
-			return err
-		}
-
-		if coreN == len(mod.cores) {
-			mod.cores = append(mod.cores, cpuCore{})
-		}
-
-		freq, err = fileAtoi(fmt.Sprintf("/sys/devices/system/cpu/cpu%d/cpufreq/scaling_cur_freq", coreN))
-		if err != nil {
-			return err
-		}
-
-		idle = 0
-		sum = 0
-
-		for idx, numStr = range fields[1:] {
-			if idx == 7 {
-				break
-			}
-
-			num, err = strconv.Atoi(numStr)
-			if err != nil {
-				return err
-			}
-
-			if idx == 3 || idx == 4 {
-				idle += num
-			}
-
-			sum += num
-		}
-
-		delta = sum - mod.cores[coreN].sum
-		mod.cores[coreN].Usage = float64(delta-(idle-mod.cores[coreN].idle)) / float64(delta) * 100
-		mod.cores[coreN].Freq = freq
-		mod.cores[coreN].idle = idle
-		mod.cores[coreN].sum = sum
-	}
-
-	if scanner.Err() != nil {
-		return err
-	}
-
-	err = stat.Close()
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
 
