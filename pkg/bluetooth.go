3356e037b08b0ac5035087ba1e4f6f5ff05dd343
andrieee44 <andrieee44@gmail.com>
2024-07-27 08:10:31 +0800

    Bluetooth module is complete

--- pkg/bluetooth.go
+++ pkg/bluetooth.go
@@ -2,17 +2,18 @@ package jstat
 
 import (
 	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/godbus/dbus/v5"
 )
 
 type bluetoothDevice struct {
-	Name, Icon            string
-	Battery, Scroll       int
-	HasBattery, Connected bool
+	Name, Icon      string
+	Battery, Scroll int
+	Connected       bool
 
-	changed chan struct{}
+	nameChan chan<- string
 }
 
 type bluetoothAdapter struct {
@@ -31,8 +32,8 @@ type bluetooth struct {
 
 	sysbus      *dbus.Conn
 	adapters    map[dbus.ObjectPath]*bluetoothAdapter
-	events      chan *dbus.Signal
 	updatesChan chan struct{}
+	events      chan *dbus.Signal
 }
 
 func (mod *bluetooth) Init() error {
@@ -49,8 +50,9 @@ func (mod *bluetooth) Init() error {
 	}
 
 	mod.adapters = make(map[dbus.ObjectPath]*bluetoothAdapter)
+	mod.updatesChan = make(chan struct{}, 10)
 	mod.events = make(chan *dbus.Signal, 10)
-	mod.updatesChan = make(chan struct{}, 1)
+	mod.sysbus.Signal(mod.events)
 
 	err = mod.sysbus.Object("org.bluez", "/").Call("org.freedesktop.DBus.ObjectManager.GetManagedObjects", 0).Store(&objects)
 	if err != nil {
@@ -63,10 +65,12 @@ func (mod *bluetooth) Init() error {
 	}
 
 	for path, object = range objects {
-		mod.updateAdapter(path, object)
-		mod.addDevice(path, object)
+		err = mod.addAdapter(path, object)
+		if err != nil {
+			return err
+		}
 
-		err = mod.sysbus.AddMatchSignal(dbus.WithMatchObjectPath(path), dbus.WithMatchInterface("org.freedesktop.DBus.Properties"), dbus.WithMatchMember("PropertiesChanged"))
+		err = mod.addDevice(path, object)
 		if err != nil {
 			return err
 		}
@@ -86,9 +90,50 @@ func (mod *bluetooth) Run() (json.RawMessage, error) {
 }
 
 func (mod *bluetooth) Sleep() error {
+	var (
+		signal  *dbus.Signal
+		iface   string
+		path    dbus.ObjectPath
+		object  map[string]map[string]dbus.Variant
+		members map[string]dbus.Variant
+		err     error
+	)
+
 	select {
 	case <-mod.updatesChan:
-	case _ = <-mod.events:
+	case signal = <-mod.events:
+		switch signal.Name {
+		case "org.freedesktop.DBus.ObjectManager.InterfacesAdded":
+			err = dbus.Store(signal.Body, &path, &object)
+			if err != nil {
+				return err
+			}
+
+			err = mod.addAdapter(path, object)
+			if err != nil {
+				return err
+			}
+
+			err = mod.addDevice(path, object)
+			if err != nil {
+				return err
+			}
+		case "org.freedesktop.DBus.Properties.PropertiesChanged":
+			err = dbus.Store(signal.Body, &iface, &members, &[]string{})
+			if err != nil {
+				return err
+			}
+
+			err = mod.updateAdapter(signal.Path, iface, members)
+			if err != nil {
+				return err
+			}
+
+			err = mod.updateDevice("", signal.Path, iface, members)
+			if err != nil {
+				return err
+			}
+		}
 	}
 
 	return nil
@@ -98,11 +143,115 @@ func (mod *bluetooth) Cleanup() error {
 	return mod.sysbus.Close()
 }
 
-func (mod *bluetooth) updateAdapter(path dbus.ObjectPath, object map[string]map[string]dbus.Variant) error {
+func (mod *bluetooth) getAdapter(path dbus.ObjectPath) (dbus.ObjectPath, error) {
+	var (
+		adapterPath dbus.ObjectPath
+		ok          bool
+	)
+
+	for adapterPath = range mod.adapters {
+		_, ok = mod.adapters[adapterPath].Devices[path]
+		if ok {
+			return adapterPath, nil
+		}
+	}
+
+	return "", fmt.Errorf("%q: adapter not found", path)
+}
+
+func (mod *bluetooth) updateAdapter(path dbus.ObjectPath, iface string, members map[string]dbus.Variant) error {
 	var (
 		name        string
 		powered, ok bool
 		err         error
+	)
+
+	if iface != "org.bluez.Adapter1" {
+		return nil
+	}
+
+	_, ok = members["Name"]
+	if ok {
+		err = members["Name"].Store(&name)
+		if err != nil {
+			return err
+		}
+
+		mod.adapters[path].Name = name
+		mod.adapters[path].nameChan <- name
+	}
+
+	_, ok = members["Powered"]
+	if ok {
+		err = members["Powered"].Store(&powered)
+		if err != nil {
+			return err
+		}
+
+		mod.adapters[path].Powered = powered
+	}
+
+	return nil
+}
+
+func (mod *bluetooth) updateDevice(adapter, path dbus.ObjectPath, iface string, members map[string]dbus.Variant) error {
+	var (
+		name          string
+		connected, ok bool
+		percentage    int
+		err           error
+	)
+
+	if iface != "org.bluez.Device1" && iface != "org.bluez.Battery1" {
+		return nil
+	}
+
+	if adapter == "" {
+		adapter, err = mod.getAdapter(path)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, ok = members["Name"]
+	if ok {
+		err = members["Name"].Store(&name)
+		if err != nil {
+			return err
+		}
+
+		mod.adapters[adapter].Devices[path].Name = name
+		mod.adapters[adapter].Devices[path].nameChan <- name
+	}
+
+	_, ok = members["Connected"]
+	if ok {
+		err = members["Connected"].Store(&connected)
+		if err != nil {
+			return err
+		}
+
+		mod.adapters[adapter].Devices[path].Connected = connected
+	}
+
+	_, ok = members["Percentage"]
+	if ok {
+		err = members["Percentage"].Store(&percentage)
+		if err != nil {
+			return err
+		}
+
+		mod.adapters[adapter].Devices[path].Battery = percentage
+		mod.adapters[adapter].Devices[path].Icon = icon(mod.icons, 100, float64(percentage))
+	}
+
+	return nil
+}
+
+func (mod *bluetooth) addAdapter(path dbus.ObjectPath, object map[string]map[string]dbus.Variant) error {
+	var (
+		ok  bool
+		err error
 	)
 
 	_, ok = object["org.bluez.Adapter1"]
@@ -110,35 +259,62 @@ func (mod *bluetooth) updateAdapter(path dbus.ObjectPath, object map[string]map[
 		return nil
 	}
 
-	err = object["org.bluez.Adapter1"]["Name"].Store(&name)
+	mod.adapters[path] = new(bluetoothAdapter)
+	mod.adapters[path].nameChan = scrollEvent(mod.updatesChan, &mod.adapters[path].Scroll, mod.scrollInterval, mod.limit)
+
+	err = mod.updateAdapter(path, "org.bluez.Adapter1", object["org.bluez.Adapter1"])
 	if err != nil {
 		return err
 	}
 
-	err = object["org.bluez.Adapter1"]["Powered"].Store(&powered)
-	if err != nil {
-		return err
-	}
+	err = mod.sysbus.AddMatchSignal(dbus.WithMatchObjectPath(path), dbus.WithMatchInterface("org.freedesktop.DBus.Properties"), dbus.WithMatchMember("PropertiesChanged"))
 
-	if mod.adapters[path] == nil {
-		mod.adapters[path] = &bluetoothAdapter{
-			Devices: make(map[dbus.ObjectPath]*bluetoothDevice),
-		}
-
-		mod.adapters[path].nameChan = scrollEvent(mod.updatesChan, &mod.adapters[path].Scroll, mod.scrollInterval, mod.limit)
-	}
-
-	if mod.adapters[path].Name != name {
-		mod.adapters[path].Name = name
-		mod.adapters[path].nameChan <- name
-	}
-
-	mod.adapters[path].Powered = powered
-
-	return nil
+	return err
 }
 
-func (mod *bluetooth) addDevice(path dbus.ObjectPath, object map[string]map[string]dbus.Variant) {
+func (mod *bluetooth) addDevice(path dbus.ObjectPath, object map[string]map[string]dbus.Variant) error {
+	var (
+		ok         bool
+		adapter    dbus.ObjectPath
+		percentage int
+		err        error
+	)
+
+	_, ok = object["org.bluez.Device1"]
+	if !ok {
+		return nil
+	}
+
+	err = object["org.bluez.Device1"]["Adapter"].Store(&adapter)
+	if err != nil {
+		return err
+	}
+
+	_, ok = object["org.bluez.Battery1"]
+	if ok {
+		err = object["org.bluez.Battery1"]["Percentage"].Store(&percentage)
+		if err != nil {
+			return err
+		}
+	}
+
+	if mod.adapters[adapter].Devices == nil {
+		mod.adapters[adapter].Devices = make(map[dbus.ObjectPath]*bluetoothDevice)
+	}
+
+	mod.adapters[adapter].Devices[path] = new(bluetoothDevice)
+	mod.adapters[adapter].Devices[path].nameChan = scrollEvent(mod.updatesChan, &mod.adapters[adapter].Devices[path].Scroll, mod.scrollInterval, mod.limit)
+	mod.adapters[adapter].Devices[path].Battery = percentage
+	mod.adapters[adapter].Devices[path].Icon = icon(mod.icons, 100, float64(percentage))
+
+	err = mod.updateDevice(adapter, path, "org.bluez.Device1", object["org.bluez.Device1"])
+	if err != nil {
+		return err
+	}
+
+	err = mod.sysbus.AddMatchSignal(dbus.WithMatchObjectPath(path), dbus.WithMatchInterface("org.freedesktop.DBus.Properties"), dbus.WithMatchMember("PropertiesChanged"))
+
+	return err
 }
 
 func NewBluetooth(scrollInterval time.Duration, limit int, icons []string) *bluetooth {
